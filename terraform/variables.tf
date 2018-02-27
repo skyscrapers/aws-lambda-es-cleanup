923f8b38d08b43a8da6140f82500737858f852c4
Mattias Gees <mattias.gees@gmail.com>
2018-02-27 16:20:05 +0100

    pick vpc changes into master

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,3 +25,14 @@ variable "python_version" {
 variable "lambda_logs_retention_in_days" {
   default = 30
 }
+
+variable "subnet_ids" {
+  description = "Subnet IDs you want to deploy the lambda in. Only fill this in if you want to deploy your Lambda function inside a VPC."
+  type        = "list"
+  default     = []
+}
+
+variable "elasticsearch_sg_id" {
+  description = "Security group ID of the AWS elasticsearch service. Only fill this in if you deploy Lambda function inside a VPC."
+  default     = ""
+}
